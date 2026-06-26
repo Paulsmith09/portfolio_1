c0285aaa51324c35d6c2478f05b098b19c8c04b2
Paulsmith09 <paulviews99@gmail.com>
2026-06-26 11:54:52 +0530

    first version

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import AboutImg from '../assets/about.png';
+
+export default function About () {
+    const config  = {
+        line1: 'A passionate Full-Stack Web Developer specializing in React.js, Tailwind CSS, Bootstrap, and Django',
+        line2: 'I build responsive, user-friendly, and scalable web applications with clean code and modern technologies.',
+        line3: `'I also have experience working with SQLite and PostgreSQL, and I'm always eager to learn and grow as a developer.'`
+    }
+
+    return <section className='flex flex-col md:flex-row bg-secondary px-5' id='about'>
+        <div className='py-5 md:w-1/2'>
+            <img src={AboutImg} />
+        </div>
+        <div className='md:w-1/2 flex justify-center'>
+            <div className='flex flex-col justify-center text-white'>
+                <h1 className='text-4xl border-b-4 border-primary mb-5 w-[170px] font-bold'>About Me</h1>
+                <p className='pb-5'>{config.line1}</p>
+                <p className='pb-5'>{config.line2}</p>
+                <p className='pb-5'>{config.line3}</p>
+            </div>
+        </div>
+    </section>
+}
